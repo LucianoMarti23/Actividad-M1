51fc9073d1ec5330871841f4969c652afc2c9136
Luciano Marti <111542636+LucianoMarti23@users.noreply.github.com>
2023-10-21 19:57:49 -0300

    Add files via upload

---
+++ Actividad-M1/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numUno = 10;  // Declaro la primer variable;
+
+        int numDos = 20; // Declaro la segunda variable;
+
+        int resultado = numDos + numUno; // Hago la correspondiente operacion y Almaceno su resultado en otra variable
+
+        System.out.println("El resultado es : " + resultado); // Imprimo por consola el resultado de la suma
+
+
+
+        // Segunda forma de operar  y imprimir por consola
+
+        System.out.println("El resultado es : " + (numUno+numDos));
+
+
+        // En la segunda forma no guardamos el resultado de la variable, esa es la diferencia de la primera
+        // Lo cual es mas adecuada cuando nuestro calculo no lo requerimos para otras operaciones o no requerimos guardarlo
+        // en memoria.
+        }
+    }
